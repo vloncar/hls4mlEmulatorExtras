e07dc9d0342701e28699243e5a21ba0174c02dc3
Andrew Loeliger <loeliger@wisc.edu>
2023-02-09 15:39:00 -0600

    Make dlopen and dlsym failures explicitly throw exceptions

--- emulator.h
+++ emulator.h
@@ -37,14 +37,14 @@ public:
         model_lib_ = dlopen(model_name_.c_str(), RTLD_LAZY);
         if (!model_lib_) {
             std::cerr << "Cannot load library: " << dlerror() << std::endl;
-            return nullptr;
+            throw "hls4ml emulator load_model() failure!";
         }
         
         create_model_cls* create_model = (create_model_cls*) dlsym(model_lib_, "create_model");
         const char* dlsym_error = dlerror();
         if (dlsym_error) {
             std::cerr << "Cannot load symbol 'create_model': " << dlsym_error << std::endl;
-            return nullptr;
+            throw "hls4ml emulator create_model() failure!";
         }
         
         model_ = create_model();
@@ -57,6 +57,7 @@ public:
         const char* dlsym_error = dlerror();
         if (dlsym_error) {
             std::cerr << "Cannot load symbol destroy_model: " << dlsym_error << std::endl;
+            throw "hls4ml emulator destroy_model() failure!";
         }
         if (model_ != nullptr) {
             destroy(model_);
